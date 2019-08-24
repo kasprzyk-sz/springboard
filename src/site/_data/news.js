48a9fd3e29730278571c21f4b9585c1d0e9a6e19
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2019-08-24 13:17:22 +0200

    setting up 2

---
+++ src/site/_data/news.js
@@ -0,0 +1,43 @@
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+module.exports = async function() {
+  var url = 'https://intranet.embl.de/communication_outreach/internal_news/2019/index.html';
+  return new Promise((resolve, reject) => {
+    // go get it
+    axios.get(url).then(response => {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      let devtoList = [];
+
+      $('.component_newsList').each(function(i, elem) {
+        devtoList[i] = {
+          title: $(this)
+            .find('div p span.title')
+            .text()
+            .trim(),
+          description: $(this)
+            .find('div p span.description')
+            .text()
+            .trim(),
+          url: $(this)
+            .find('div p a')
+            .attr('href'),
+          date: $(this)
+            .find('div p span.place_date')
+            .text()
+            .trim(),
+          image: $(this)
+            .find('a img')
+            .attr('src'),
+        };
+      });
+
+
+      const devtoListTrimmed = devtoList.filter(n => n != undefined);
+      // we've giot all the data now. So resolve the promise to return the data
+      // we've giot all the data now. So resolve the promise to return the data
+      resolve({ items: devtoListTrimmed.slice(0,1)  });
+    }); //end then
+  }); //end promise
+};
---
+++ src/site/_data/seminars.js
@@ -0,0 +1,48 @@
+var axios = require('axios');
+var toJSON = require('xml2js').parseString;
+
+var url = 'http://www-db.embl.de/jss/EmblGroupsCMS/events_0?dutystation=1&timeFrame=FUTURE&seminarTypeID=0';
+
+module.exports = () => {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then((response) => {
+        // turn the feed XML into JSON
+        toJSON(response.data, function(err, result) {
+
+          // create a path for each item based on Medium's guid URL
+          // result.events.forEach(element => {
+          //     seminar_names['element'] = element
+          // });
+          var seminars = [];
+          //create a path for each item based on Medium's guid URL
+          result['events']['seminars'][0]['seminar'].forEach(element => {
+            var item = {
+              'org_name': element.emblOrgName[0],
+              'color': element.color[0],
+              'title': element.title[0],
+              'date': element.date[0],
+              'room': element.room[0],
+              'firstname': element.firstname[0],
+              'name': element.name[0]
+              
+            }
+
+            seminars.push(item);
+
+          });
+
+          console.log(seminars);
+
+
+          resolve({
+            'url': url,
+            'items': seminars.slice(0,3) // get the first 3 only
+          });
+        });
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+};
